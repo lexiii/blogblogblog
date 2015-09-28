8e3f0cd5084c19499d789b72b3e47b5188bac042
Lexi <leck.she@gmail.com>
2015-09-28 16:33:45 +1000

    better error messages

--- connection.php
+++ connection.php
@@ -8,9 +8,14 @@
 
         public static function getInstance() {
             if(!isset(self::$instance)) {
-                $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+                try {
+                    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
                     self::$instance = new PDO('mysql:host=localhost;dbname=blogthing',root,root, $pdo_options);
+                } catch (PDOException $e) {
+                    echo 'Connection failed: ' . $e->getMessage();
+                    die();
+                }
             }
             return self::$instance;
         }
